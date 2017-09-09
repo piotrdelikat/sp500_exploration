ce83c7b7e5a4407515dea4f207b8a321e9258eb9
Piotr Delikat <piotrdellikat@gmail.com>
2017-09-09 20:32:29 +0200

    Min, Max, Mean, first plot

--- s&p500.py
+++ s&p500.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 def table_to_pickle():
     url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
     html = pd.read_html(url,skiprows=0 ,header=0, index_col=3)
@@ -12,6 +12,8 @@ def table_to_pickle():
 #Preparing data
 info = pd.read_pickle('sp500_companies.pickle')
 prices = pd.read_csv('sp500_joined_closes.csv', index_col=0)
+industries = info.index.drop_duplicates().tolist()
+
 
 time_format = '%Y-%m-%d'
 prices.index = pd.to_datetime(prices.index, format=time_format)
@@ -23,9 +25,11 @@ tickers = industry['Ticker symbol'].tolist()
 
 prices_of_industry = prices[tickers]
 
-# max and min for visualisation
 data = pd.DataFrame()
-# data[max] =
-# data[min] =
+data['min'] = prices_of_industry.min(axis=1)
+data['max'] = prices_of_industry.max(axis=1)
+data['mean'] = prices_of_industry.mean(axis=1)
 
-
+# Plot
+data.plot()
+plt.show()
