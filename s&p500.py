2cdece2c0051c86e2e03a71c52e506024a05c3d8
Piotr Delikat <piotrdellikat@gmail.com>
2017-09-20 21:40:02 +0200

    DataFrame and plot of mean values of every industry

--- s&p500.py
+++ s&p500.py
@@ -16,6 +16,9 @@ info = pd.read_pickle('sp500_companies.pickle')
 prices = pd.read_csv('sp500_joined_closes.csv', index_col=0)
 industries = info.index.drop_duplicates().tolist()
 
+#Filternig missing tickers
+data_present = prices.columns.tolist()
+tickers = info[info['Ticker symbol'].isin(data_present)]
 
 time_format = '%Y-%m-%d'
 prices.index = pd.to_datetime(prices.index, format=time_format)
@@ -27,24 +30,27 @@ sp500_mean = prices_monthly.mean(axis=1)
 
 def output_data():
     data = pd.DataFrame()
-    for industry in industries[:1]:
-        tickers = info.loc[industry, ['Ticker symbol']]
-        tickers = tickers['Ticker symbol'].tolist()
-        prices_of_industry = prices[tickers]
+    for industry in industries:
+        global tickers
+        ticks = tickers.loc[industry, ['Ticker symbol']]
+        ticks = ticks['Ticker symbol'].tolist()
 
-        data['min'] = prices_of_industry.min(axis=1)
-        data['max'] = prices_of_industry.max(axis=1)
-        data['mean'] = prices_of_industry.mean(axis=1)
+        prices_of_industry = prices[ticks]
+        # data['min'] = prices_of_industry.min(axis=1)
+        # data['max'] = prices_of_industry.max(axis=1)
+        data['{}'.format(industry)] = prices_of_industry.mean(axis=1)
 
     return data
         #output = df with min max, mean, of every industry and sp500
 
 
 
-# # Plot
+# Plot
 data = output_data()
 
-plt.plot(data)
-plt.plot(sp500_mean)
+for industry in data.columns.values:
+    plt.plot(data[industry], label=industry)
+plt.plot(sp500_mean, linewidth=3.0, color='r', label='S&P500')
+plt.title('S&P500 mean vs. mean of Industries')
 plt.legend()
 plt.show()
