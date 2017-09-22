54188615cfe7e242fad2366ad119728ee96d38b5
Piotr Delikat <piotrdellikat@gmail.com>
2017-09-22 18:32:37 +0200

    Line plot with data from all columns

--- sp500_bokeh_plot.py
+++ sp500_bokeh_plot.py
@@ -1,24 +1,14 @@
 import numpy as np
 import pandas as pd
-from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure, show, output_file
+
+colors=['#1a3309', '#2153bd','#ba2a20', '#275597', '#8e43df', '#67d3b4', '#b3f2c7', '#9faa92','#a9d3d8','#3b7624','#b69cb4','#c8cf46']
 
 sp500 = pd.read_csv('sp500_industries_mean.csv',  index_col=0)
 
 time_format = '%Y-%m-%d'
 sp500.index = pd.to_datetime(sp500.index, format=time_format)
 
-all = sp500['S&P500']
-
-data = dict(
-    sp500 = all
-)
-
-# for industry in sp500.columns.values[1:]:
-#     data['{}'.format(industry)] = sp500[industry]
-
-
-source = ColumnDataSource(data=data)
 
 def datetime(x):
     return np.array(x, dtype=np.datetime64)
@@ -36,7 +26,10 @@ p.xaxis.axis_label = 'Date'
 p.yaxis.axis_label = 'Price'
 
 
-p.line(datetime(sp500.index), all, color='#A6CEE3', legend='S&P500 mean stock price')
+for i, industry in enumerate(sp500.columns.values[1:]):
+    p.line(datetime(sp500.index), sp500['{}'.format(industry)], color=colors[i], legend='{}'.format(industry))
+    # data['{}'.format(industry)] = sp500[industry]
+
 p.legend.location = "top_left"
 
 # output_file("sp500_mean.html", title="")
