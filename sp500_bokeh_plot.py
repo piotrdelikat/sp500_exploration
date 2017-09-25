288b3984c4858e240e20fe6293f68260e800865b
Piotr Delikat <piotrdellikat@gmail.com>
2017-09-25 21:13:49 +0200

    Title, color, line width

--- sp500_bokeh_plot.py
+++ sp500_bokeh_plot.py
@@ -2,36 +2,33 @@ import numpy as np
 import pandas as pd
 from bokeh.plotting import figure, show, output_file
 
-colors=['#1a3309', '#2153bd','#ba2a20', '#275597', '#8e43df', '#67d3b4', '#b3f2c7', '#9faa92','#a9d3d8','#3b7624','#b69cb4','#c8cf46']
-
+colors=['#0f0f0a', '#ff9966','#ba2a20', '#275597', '#8e43df', '#67d3b4', '#663300', '#006600','#a9d3d8','#e6e600','#b69cb4']
 sp500 = pd.read_csv('sp500_industries_mean.csv',  index_col=0)
 
 time_format = '%Y-%m-%d'
 sp500.index = pd.to_datetime(sp500.index, format=time_format)
-
+industries = sp500.columns.values[1:]
 
 def datetime(x):
     return np.array(x, dtype=np.datetime64)
 
-
 p = figure(
     plot_width=1100,
-    plot_height=700,
+    plot_height=600,
     responsive=True,
     x_axis_type="datetime",
-    title="S&P500 mean vs. mean of Industries")
-
+    title="Mean price of stocks from all sectors in S&P500 index")
 
 p.xaxis.axis_label = 'Date'
 p.yaxis.axis_label = 'Price'
 
+for name, color in zip(industries, colors):
+    p.line(datetime(sp500.index), sp500[name], alpha=0.8, color=color, line_width=3, legend=name)
 
-for i, industry in enumerate(sp500.columns.values[1:]):
-    p.line(datetime(sp500.index), sp500['{}'.format(industry)], color=colors[i], legend='{}'.format(industry))
-    # data['{}'.format(industry)] = sp500[industry]
+p.line(datetime(sp500.index), sp500['S&P500'], line_width=5, color='red', legend='S&P500')
 
 p.legend.location = "top_left"
 
-# output_file("sp500_mean.html", title="")
+# output_file("sp500_sectors_mean.html")
 
 show(p)  # open a browser
