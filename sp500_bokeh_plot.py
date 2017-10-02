72330a5d710cef3c57616bf6e02347c3253b41df
Piotr Delikat <piotrdellikat@gmail.com>
2017-10-02 23:42:22 +0200

    Outside legend

--- sp500_bokeh_plot.py
+++ sp500_bokeh_plot.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
+from bokeh.models import Legend
+from bokeh.layouts import layout
 from bokeh.plotting import figure, show, output_file
 
 colors=['#0f0f0a', '#ff9966','#ba2a20', '#275597', '#8e43df', '#67d3b4', '#663300', '#006600','#a9d3d8','#e6e600','#b69cb4']
+legend_items = []
 sp500 = pd.read_csv('sp500_industries_mean.csv',  index_col=0)
 
 time_format = '%Y-%m-%d'
@@ -17,17 +20,23 @@ p = figure(
     plot_height=600,
     responsive=True,
     x_axis_type="datetime",
-    title="Mean price of stocks from all sectors in S&P500 index")
+    title="Mean price of stocks from all sectors in S&P500 index",
+    toolbar_location="below",
+    toolbar_sticky=False
+)
 
 p.xaxis.axis_label = 'Date'
 p.yaxis.axis_label = 'Price'
 
 for name, color in zip(industries, colors):
-    p.line(datetime(sp500.index), sp500[name], alpha=0.8, color=color, line_width=3, legend=name)
+    plot = p.line(datetime(sp500.index), sp500[name], line_width=3, line_color=color)
+    legend_items.append((name, [plot]))
 
-p.line(datetime(sp500.index), sp500['S&P500'], line_width=5, color='red', legend='S&P500')
+index = p.line(datetime(sp500.index), sp500['S&P500'], line_width=5, line_color='red')
+legend_items.append(('S&P500', [index]))
 
-p.legend.location = "top_left"
+legend = Legend(items=legend_items, location=(0, -30))
+p.add_layout(legend, 'right')
 
 # output_file("sp500_sectors_mean.html")
 
